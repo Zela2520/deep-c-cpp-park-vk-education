5ddf2fdc0538336cacff354c55bc024462111b58
Zela2520 <sugarbeatzela@gmail.com>
2022-03-09 00:03:43 +0300

    2th commit

--- project/include/task_info.h
+++ project/include/task_info.h
@@ -5,9 +5,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <time.h>
-#define MAX_STR_SIZE 10000
+#define MAX_STR_SIZE 1024
 
+typedef enum {
+    SUCCESS,
+    FAILURE
+} response;
 
 //typedef enum {
 //    LOWEST,
@@ -27,15 +32,16 @@ typedef task_info* pointer_task_lists;
 
 void propose_action();
 
-void create_task_lists(pointer_task_lists current_task_lists);
+pointer_task_lists create_task_lists(pointer_task_lists current_task_lists);
+void print_task_lists(pointer_task_lists current_task_lists);
 void delete_task_lists(pointer_task_lists current_task_lists);
 
 pointer_task_lists create_task();
 int set_task(pointer_task_lists current_task);
 void print_task(pointer_task_lists current_task);
-void delete_task(pointer_task_lists task);
+//void delete_task(pointer_task_lists task);
 
-void free_all();
+//void free_all();
 
 
 #endif PROJECT_INCLUDE_TASK_INFO_ //  PROJECT_INCLUDE_TASK_INFO_
