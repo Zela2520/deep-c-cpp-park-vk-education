6678441ede35241cd411503dbdc2a1871e9e44c8
Zela2520 <sugarbeatzela@gmail.com>
2022-03-09 00:03:43 +0300

    2th commit

--- project/src/main.c
+++ project/src/main.c
@@ -1,9 +1,11 @@
 #include "../include/task_info.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     propose_action();
     pointer_task_lists task_lists;
-    create_task_lists(task_lists);
+    if (create_task_lists(task_lists) == NULL) {
+        perror("Failed to create task list");
+    }
 
     delete_task_lists(task_lists);
     return 0;
