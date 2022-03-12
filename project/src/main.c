6bab561b1a210445b7c2ba252f4269ad5d5b75d8
Zela2520 <sugarbeatzela@gmail.com>
2022-03-12 19:40:30 +0300

    Added reading from a file. 18 commit.

--- project/src/main.c
+++ project/src/main.c
@@ -1,22 +1,33 @@
 #include "../include/task_list.h"
 
-int main(void) {
+int main(int argc, const char **argv) {
+    if (argc != 2) {
+        perror("input path error");
+        return ERROR;
+    }
+
+    const char *input_stream_path = argv[1];
+
     list* user_list = create_list(DEFAULT_SIZE);
 
     if (user_list == NULL) {
         perror("creat list error");
+        return ERROR;
     }
 
-    if (business_logic(user_list)) {
+    if (business_logic(user_list, input_stream_path)) {
         perror("business logic error");
+        return ERROR,
     }
 
     if (print_list(user_list)) {
         perror("print list error");
+        return ERROR;
     }
 
     if (free_list(user_list)) {
         perror("memory free error");
+        return ERROR;
     }
 
     return 0;
