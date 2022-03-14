113bd1cb6ea1ee47a3c6057701b5b6b4079eaec8
Zela2520 <sugarbeatzela@gmail.com>
2022-03-14 19:58:07 +0300

    everything is ready for testing. 30 commit.

--- project/src/main.c
+++ project/src/main.c
@@ -16,11 +16,25 @@ int main(int argc, const char **argv) {
     }
 
     if (business_logic(user_list, input_stream_path)) {
+        if (free_list(user_list)) {
+            perror("memory free error");
+        }
         perror("business logic error");
         return ERROR,
     }
 
+//    if (!(sort_list(user_list))) {
+//        if (free_list(user_list)) {
+//            perror("memory free error");
+//        }
+//        perror("sort_list error");
+//        return ERROR,
+//    }
+
     if (print_list(user_list)) {
+        if (free_list(user_list)) {
+            perror("memory free error");
+        }
         perror("print list error");
         return ERROR;
     }
