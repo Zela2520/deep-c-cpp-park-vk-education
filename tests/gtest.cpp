78b631ff30740291b4b4f7b303b6053e53101120
Zela2520 <sugarbeatzela@gmail.com>
2022-03-15 21:10:57 +0300

    the second test has been fixed. 58 commit

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -19,9 +19,9 @@ TEST(TASK_LIST_TEST, creat_list_data) {
     new_list = (list*) calloc(INCREMENT, sizeof(list));
     EXPECT_FALSE(new_list == NULL);
     new_list->data = NULL;
-    EXPECT_TRUE(new_list == NULL);
+    EXPECT_TRUE(new_list->data == NULL);
     EXPECT_EQ(create_list_data(new_list), SUCCESS);
-    EXPECT_FALSE(new_list == NULL);
+    EXPECT_FALSE(new_list->data == NULL);
     EXPECT_EQ(free_list(new_list), SUCCESS);
 }
 
