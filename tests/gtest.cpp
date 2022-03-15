6f96565225f096c2e79c527636dd4bd52b8d815b
Zela2520 <sugarbeatzela@gmail.com>
2022-03-15 19:18:13 +0300

    first test has been added. 53 commit

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -5,7 +5,14 @@ extern "C" {
     #include "../include/task_list.h"
 }
 
-
+TEST(TASK_LIST_TEST, create_list) {
+    list* new_list = NULL;
+    EXPECT_TRUE(new_list == NULL);
+    new_list = create_list(DEFAULT_SIZE);
+    EXPECT_FALSE(new_list == NULL);
+    free_list(new_list);
+    EXPECT_TRUE(new_list == NULL);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
