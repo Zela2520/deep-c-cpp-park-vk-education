d23b724f38e187965d974962b36c73d1080d4b30
Zela2520 <sugarbeatzela@gmail.com>
2022-03-15 21:06:40 +0300

    the second test has been added. 56 commit

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -10,7 +10,19 @@ TEST(TASK_LIST_TEST, create_list) {
     EXPECT_TRUE(new_list == NULL);
     new_list = create_list(DEFAULT_SIZE);
     EXPECT_FALSE(new_list == NULL);
-    free_list(new_list);
+    EXPECT_EQ(free_list(new_list), SUCCESS);
+}
+
+TEST(TASK_LIST_TEST, creat_list_data) {
+    list* new_list = NULL;
+    EXPECT_TRUE(new_list == NULL);
+    new_list = (list*) calloc(INCREMENT, sizeof(list));
+    EXPECT_FALSE(new_list == NULL);
+    new_list->data = NULL;
+    EXPECT_TRUE(new_list == NULL);
+    EXPECT_EQ(create_list_data(new_list), SUCCESS);
+    EXPECT_FALSE(new_list == NULL);
+    EXPECT_EQ(free_list(new_list), SUCCESS);
 }
 
 int main(int argc, char **argv) {
