df380484a987d606f2bb77164d71d0d6584a5e1d
Zela2520 <sugarbeatzela@gmail.com>
2022-03-16 12:47:00 +0300

    added sort list function test

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -83,19 +83,63 @@ TEST(TASK_LIST_TEST, put_elem) {
     EXPECT_EQ(free_list(new_list), SUCCESS);
 }
 
-//TEST(TASK_LIST_TEST, business_logic) {
-//    list* new_list = create_list(DEFAULT_SIZE);
-//    EXPECT_EQ(business_logic(NULL, "./start_server"), ERROR);
-//    EXPECT_EQ(business_logic(new_list, NULL), ERROR);
-//    EXPECT_EQ(propose_action(NULL), ERROR);
-//    EXPECT_EQ(get_string(NULL), NULL);
-//    EXPECT_EQ(get_symbol(NULL), '\0');
-//    EXPECT_EQ(make_choice(NULL), ERROR);
-//
-//    ptr_task new_task = NULL;
-//    EXPECT_TRUE(new_task == NULL);
-//
-//}
+TEST(TASK_LIST_TEST, sort_list) {
+    list* new_list = create_list(DEFAULT_SIZE);
+    ptr_task first_task = create_task();
+    ptr_task second_task = create_task();
+    ptr_task third_task = create_task();
+
+    strcpy(first_task->number, "1");
+    strcpy(first_task->description[0], "hello");
+    strcpy(first_task->priority, "2");
+    first_task->when->number = 28;
+    first_task->when->month = 4;
+    first_task->when->year = 2020;
+
+    strcpy(second_task->number, "2");
+    strcpy(second_task->description[0], "world");
+    strcpy(second_task->priority, "5");
+    second_task->when->number = 20;
+    second_task->when->month = 4;
+    second_task->when->year = 2020;
+
+    strcpy(third_task->number, "3");
+    strcpy(third_task->description[0], "!!!");
+    strcpy(third_task->priority, "5");
+    third_task->when->number = 18;
+    third_task->when->month = 4;
+    third_task->when->year = 2020;
+
+    put_elem(new_list, first_task);
+    put_elem(new_list, second_task);
+    put_elem(new_list, third_task);
+
+    EXPECT_FALSE(sort_list(new_list) == NULL);
+    EXPECT_EQ(new_list->insert_pos, 3);
+
+    EXPECT_EQ(atoi(new_list->data[0]->number), 3);
+    EXPECT_FALSE(new_list->data[0]->description == NULL);
+    EXPECT_EQ(atoi(new_list->data[0]->priority), 5);
+    EXPECT_EQ(new_list->data[0]->when->number, 18);
+    EXPECT_EQ(new_list->data[0]->when->month, 4);
+    EXPECT_EQ(new_list->data[0]->when->year, 2020);
+
+    EXPECT_EQ(atoi(new_list->data[1]->number), 2);
+    EXPECT_FALSE(new_list->data[1]->description == NULL);
+    EXPECT_EQ(atoi(new_list->data[1]->priority), 5);
+    EXPECT_EQ(new_list->data[1]->when->number, 20);
+    EXPECT_EQ(new_list->data[1]->when->month, 4);
+    EXPECT_EQ(new_list->data[1]->when->year, 2020);
+
+    EXPECT_EQ(atoi(new_list->data[2]->number), 1);
+    EXPECT_FALSE(new_list->data[2]->description == NULL);
+    EXPECT_EQ(atoi(new_list->data[2]->priority), 2);
+    EXPECT_EQ(new_list->data[2]->when->number, 28);
+    EXPECT_EQ(new_list->data[2]->when->month, 4);
+    EXPECT_EQ(new_list->data[2]->when->year, 2020);
+
+    EXPECT_EQ(free_list(new_list), SUCCESS);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
