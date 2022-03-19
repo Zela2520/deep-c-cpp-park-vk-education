92f246c1296a274c4bc712f16ffc1dc80eb719d3
Zela2520 <sugarbeatzela@gmail.com>
2022-03-19 23:12:41 +0300

    corrected some comments 12

--- src/list/task_list_modify.c
+++ src/list/task_list_modify.c
@@ -28,9 +28,9 @@ static int increase_list(list* tasks, const size_t str_len) {
             return ERROR;
         }
 
-        for (size_t i = tasks->size; i < tasks->capasity; ++i) {
-            memset(&tasks->data[i], 0, sizeof(tasks->data[i]));
-        }
+//        for (size_t i = tasks->size; i < tasks->capasity; ++i) {
+//            memset(&tasks->data[i], 0, sizeof(tasks->data[i]));
+//        }
 
         tasks->data = new_data;
     }
