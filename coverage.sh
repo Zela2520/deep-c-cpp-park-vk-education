702947ab1bab6bda8abc6ff9be28be8453bb4a0b
Zela2520 <sugarbeatzela@gmail.com>
2022-03-28 01:51:02 +0300

    began building infrastructure 2

--- coverage.sh
+++ coverage.sh
@@ -3,5 +3,5 @@
 set -e
 ./build/tests/main_test
 
-lcov -t "build/tests/matrix_test" -o coverage.info -c -d build/project/CMakeFiles/main_lib.dir/
+lcov -t "build/tests/matrix_test" -o coverage.info -c -d build/project/CMakeFiles/matrix_test.dir/
 genhtml -o "build/report/" coverage.info
--- project/matrix/matrix.h
+++ project/matrix/matrix.h
@@ -17,10 +17,4 @@ Matrix* create_matrix_from_file(const char* path_file);
 Matrix* create_matrix(size_t rows, size_t cols);
 void free_matrix(Matrix* matrix);
 
-// Basic operations
-int get_rows(const Matrix* matrix, size_t* rows);
-int get_cols(const Matrix* matrix, size_t* cols);
-int get_elem(const Matrix* matrix, size_t row, size_t col, double* val);
-int set_elem(Matrix* matrix, size_t row, size_t col, double val);
-
 #endif  // PROJECT_MATRIX_H_
