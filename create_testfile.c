3a19e32156191c9ff118c81dbf985a9937bd3d02
Zela2520 <sugarbeatzela@gmail.com>
2022-03-28 12:46:56 +0300

    began building infrastructure 10

--- create_testfile.c
+++ create_testfile.c
@@ -1,11 +1,40 @@
 #include "project/matrix/matrix.h"
 
-#define OU
+#define INT_SIZE 2
+#define DOUBLE_SIZE 8
 
+static int fill_file(FILE* file_input, Matrix* cur_matrix) {
+    int arg_count = 0;
+    bool error_flag = 0;
 
-void fill_file(FILE* file_input, Matrix* cur_matrix) {
+    for (size_t current_row = 0; current_row < cur_matrix->rows; ++current_row) {
+        for (size_t current_col = 0; current_col < cur_matrix->columns; ++current_col) {
+            arg_count = fprintf(file_input, "%lf ",
+                                cur_matrix->matrix[current_row * cur_matrix->columns + current_col]);
+            if (arg_count != 1 * DOUBLE_SIZE) {
+                printf("%d\n", arg_count);
+                error_flag = 1;
+                break;
+            }
+        }
 
+        arg_count = fprintf(file_input, "%c", '\n');
+        if (arg_count != 1) {
+            error_flag = 1;
+            break;
+        }
+    }
+
+    fclose(file_input);
+
+    if (error_flag) {
+        free_matrix(cur_matrix);
+        return -1;
+    }
+
+    return 0;
 }
+
 
 int main(void) {
      Matrix *cur_matrix = create_matrix(MEDIUM_ROWS, MEDIUM_COLS);
@@ -20,8 +49,8 @@ int main(void) {
          return -1;
      }
 
-     int ret_val = fprintf(file_input, "%d%c", MEDIUM_ROWS, '\n');
-     if (ret_val != 4) {
+     int ret_val = fprintf(file_input, "%d ", MEDIUM_ROWS);
+     if (ret_val != 2 * INT_SIZE) {
          printf("%d\n", ret_val);
          perror("writing error");
          free_matrix(cur_matrix);
@@ -30,7 +59,7 @@ int main(void) {
      }
 
      ret_val = fprintf(file_input, "%d%c", MEDIUM_COLS, '\n');
-     if (ret_val != 4) {
+     if (ret_val != 2 * INT_SIZE) {
          printf("%d\n", ret_val);
          perror("writing error");
          free_matrix(cur_matrix);
@@ -38,33 +67,11 @@ int main(void) {
          return -1;
      }
 
-     int arg_count = 0;
-     bool error_flag = 0;
-
-     for (size_t current_row = 0; current_row < cur_matrix->rows; ++current_row) {
-         for (size_t current_col = 0; current_col < cur_matrix->columns; ++current_col) {
-             arg_count = fprintf(file_input, "%lf ",
-                                 cur_matrix->matrix[current_row * cur_matrix->columns + current_col]);
-             if (arg_count != 8) {
-                 printf("%d\n", arg_count);
-                 error_flag = 1;
-                 break;
-             }
-         }
-
-         arg_count = fprintf(file_input, "%c", '\n');
-         if (arg_count != 1) {
-             error_flag = 1;
-             break;
-         }
-     }
-
-     fclose(file_input);
-
-     if (error_flag) {
-         free_matrix(cur_matrix);
+     if (fill_file(file_input, cur_matrix)) {
+         perror("file_file error");
          return -1;
      }
+
 
      free_matrix(cur_matrix);
 
