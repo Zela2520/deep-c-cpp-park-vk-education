24b2e82929a6505b64a43e246afce7765aa59b4f
Zela2520 <sugarbeatzela@gmail.com>
2022-03-29 00:03:28 +0300

    fill matrix

--- create_testfile.c
+++ create_testfile.c
@@ -5,21 +5,26 @@ static int fill_file(FILE* file_input, Matrix* cur_matrix) {
     int arg_count = 0;
     bool error_flag = 0;
 
+    if (!(set_random_values(cur_matrix))) {
+        return ERROR;
+    }
+
     for (size_t current_row = 0; current_row < cur_matrix->rows; ++current_row) {
         for (size_t current_col = 0; current_col < cur_matrix->columns; ++current_col) {
 
-            arg_count = fprintf(file_input, "%lf",
+            arg_count = fprintf(file_input, "%-10lf",
                                 cur_matrix->matrix[current_row * cur_matrix->columns + current_col]);
 
             if (current_col + 1 != cur_matrix->columns) {
                 fprintf(file_input, "%c", ' ');
             }
 
-            if (arg_count != 1 * DOUBLE_SIZE) {
-                perror("writting error");
-                error_flag = 1;
-                break;
-            }
+//            if (arg_count != 1 + DOUBLE_SIZE) {
+//                printf("%d\n", arg_count);
+//                perror("third writting error");
+//                error_flag = 1;
+//                break;
+//            }
         }
 
         if (current_row + 1 != cur_matrix->rows) {
@@ -66,7 +71,7 @@ int main(int argc, char *argv[]) {
     int ret_val = fprintf(file_input, "%d ", 3); // так удобнее отлаживать
     if (ret_val != 1 + 1) {
         printf("%d\n", ret_val);
-        perror("writing error");
+        perror("first writing error");
         free_matrix(cur_matrix);
         fclose(file_input);
         return ERROR;
@@ -75,7 +80,7 @@ int main(int argc, char *argv[]) {
     ret_val = fprintf(file_input, "%d\n", 6);
     if (ret_val != (1 + 1)) {
         printf("%d\n", ret_val);
-        perror("writing error");
+        perror("second writing error");
         free_matrix(cur_matrix);
         fclose(file_input);
         return ERROR;
--- project/matrix/matrix_basic.c
+++ project/matrix/matrix_basic.c
@@ -12,3 +12,28 @@ int set_elem(Matrix* matrix, size_t row, size_t col, double val) {
     matrix->matrix[row * matrix->columns + col] = val;
     return 0;
 }
+
+double* set_random_values(Matrix* user_matrix) {
+    if (user_matrix == NULL || user_matrix->matrix == NULL) {
+        perror("set_random_values");
+        return NULL;
+    }
+
+    srand(time(NULL));
+
+    bool error_flag = 0;
+    for (size_t current_row = 0; current_row < user_matrix->rows; ++current_row) {
+        for (size_t current_col = 0; current_col < user_matrix->columns; ++current_col) {
+            if (set_elem(user_matrix, current_row, current_col, rand() % 10 + 1)) {
+                perror("set_random_values set elem");
+                error_flag = 1;
+            };
+        }
+    }
+
+    if (error_flag) {
+        return NULL;
+    }
+
+    return user_matrix->matrix;
+}
