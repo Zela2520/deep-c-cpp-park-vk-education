19f450e54debce6aa763de6f4b8619535e48ade9
Zela2520 <sugarbeatzela@gmail.com>
2022-03-28 10:11:57 +0300

    began building infrastructure 9

--- create_testfile.c
+++ create_testfile.c
@@ -1,6 +1,13 @@
 #include "project/matrix/matrix.h"
 
- int main(void) {
+#define OU
+
+
+void fill_file(FILE* file_input, Matrix* cur_matrix) {
+
+}
+
+int main(void) {
      Matrix *cur_matrix = create_matrix(MEDIUM_ROWS, MEDIUM_COLS);
      if (cur_matrix == NULL) {
          perror("filling testfiles error");
@@ -13,14 +20,18 @@
          return -1;
      }
 
-     if (fprintf(file_input, "%d ", MEDIUM_ROWS) != 2) {
+     int ret_val = fprintf(file_input, "%d%c", MEDIUM_ROWS, '\n');
+     if (ret_val != 4) {
+         printf("%d\n", ret_val);
          perror("writing error");
          free_matrix(cur_matrix);
          fclose(file_input);
          return -1;
      }
 
-     if (fprintf(file_input, "%d\n", MEDIUM_COLS) != 2) {
+     ret_val = fprintf(file_input, "%d%c", MEDIUM_COLS, '\n');
+     if (ret_val != 4) {
+         printf("%d\n", ret_val);
          perror("writing error");
          free_matrix(cur_matrix);
          fclose(file_input);
@@ -34,7 +45,7 @@
          for (size_t current_col = 0; current_col < cur_matrix->columns; ++current_col) {
              arg_count = fprintf(file_input, "%lf ",
                                  cur_matrix->matrix[current_row * cur_matrix->columns + current_col]);
-             if (arg_count != 2) {
+             if (arg_count != 8) {
                  printf("%d\n", arg_count);
                  error_flag = 1;
                  break;
