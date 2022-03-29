384198a344d7b926d04c723a66e0fcb9adad117b
Zela2520 <sugarbeatzela@gmail.com>
2022-03-29 10:44:57 +0300

    business logic was added

---
+++ project/business_logic.c
@@ -0,0 +1,24 @@
+#include "business_logic.h"
+
+int business_logic() {
+    Matrix* user_matrix = create_matrix_from_file("tests/test_files/normal.txt");
+    if (user_matrix == NULL) {
+        perror("create matrix error");
+        return ERROR;
+    }
+
+    double* result = calculate_sum_cols(user_matrix);
+    if (result == NULL) {
+        perror("Failed to calculate the sum of the matrix elements");
+        return ERROR;
+    }
+
+    if (print_result(user_matrix->matrix, user_matrix->columns)) {
+        perror("matrix printing error");
+        return ERROR;
+    }
+
+    free(result);
+
+    return SUCCESS;
+}
---
+++ project/business_logic.h
@@ -0,0 +1,8 @@
+#ifndef PROJECT_BUSINESS_LOGIC_H_
+#define PROJECT_BUSINESS_LOGIC_H_
+
+#include "matrix/matrix.h"
+
+int business_logic();
+
+#endif  // PROJECT_BUSINESS_LOGIC_H_
