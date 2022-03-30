28b1414b5713eb905dbaa3cfe903e6847e1871df
Zela2520 <sugarbeatzela@gmail.com>
2022-03-30 11:48:57 +0300

    fixed tests

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Hw/tests
-# Build directory: /Hw/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
