58c921b493cf085099ec1cdeb5e58b8ce095c4bb
Zela2520 <sugarbeatzela@gmail.com>
2022-03-30 11:48:57 +0300

    fixed tests

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -9,19 +9,19 @@ extern "C" {
 #define NORMAL_FILE "test_files/normal.txt"
 #define SMALL_FILE "test_files/small.txt"
 
-//TEST(MATRIX, create_matrix_from_file) {
-//    Matrix* user_matrix = NULL;
-//    EXPECT_TRUE(user_matrix == NULL);
-//
-//    user_matrix = create_matrix_from_file(NORMAL_FILE);
-//    EXPECT_FALSE(user_matrix == NULL);
-//
-//    EXPECT_FALSE(user_matrix->matrix == NULL);
-//    EXPECT_EQ(user_matrix->rows, 3);
-//    EXPECT_EQ(user_matrix->columns, 6);
-//
-//    free_matrix(user_matrix);
-//}
+TEST(MATRIX, create_matrix_from_file) {
+    Matrix* user_matrix = NULL;
+    EXPECT_TRUE(user_matrix == NULL);
+
+    user_matrix = create_matrix_from_file("test_files/normal.txt");
+    EXPECT_FALSE(user_matrix == NULL);
+
+    EXPECT_FALSE(user_matrix->matrix == NULL);
+    EXPECT_EQ(user_matrix->rows, 3);
+    EXPECT_EQ(user_matrix->columns, 6);
+
+    free_matrix(user_matrix);
+}
 
 TEST(BUSINESS_LOGIC, business_logic) {
     EXPECT_EQ(business_logic(), SUCCESS);
