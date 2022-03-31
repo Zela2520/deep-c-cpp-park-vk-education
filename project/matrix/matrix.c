35bf4ad464a554dd8d233ea9e5a3540c2a91e4f4
Zela2520 <sugarbeatzela@gmail.com>
2022-03-31 13:54:57 +0300

    refactoring structure

--- project/matrix/matrix.c
+++ project/matrix/matrix.c
@@ -73,3 +73,15 @@ void free_matrix(Matrix* matrix) {
 
     free(matrix);
 }
+
+int print_result(double* result, size_t len) {
+    if (result == NULL) {
+        perror("print result error");
+        return ERROR;
+    }
+
+    for (size_t i = 0; i < len; ++i) {
+        printf("%lf ", result[i]);
+    }
+    return SUCCESS;
+}
