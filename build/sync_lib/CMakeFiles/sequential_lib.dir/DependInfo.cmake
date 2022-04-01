1449717c1464cf2bf8c53013b35bba52f225f218
Zela2520 <sugarbeatzela@gmail.com>
2022-04-01 14:46:46 +0300

    trying to set up test coverage

---
+++ build/sync_lib/CMakeFiles/sequential_lib.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/hw/sync_lib/main.c" "/hw/build/sync_lib/CMakeFiles/sequential_lib.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../sync_lib/matrix"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/hw/build/sync_lib/CMakeFiles/sync_matrix_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CMakeFiles/sync_tests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sync_tests.dir/sync_test.cpp.o"
+  "sync_tests"
+  "sync_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sync_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
