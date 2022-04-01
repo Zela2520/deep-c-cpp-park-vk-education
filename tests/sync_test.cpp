5e0c970425821cce968eb7a2e9f63ac8d220cb8f
Zela2520 <sugarbeatzela@gmail.com>
2022-04-01 10:10:07 +0300

    corrected cmakelists.txt

---
+++ tests/sync_test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "../sync_lib/matrix/matrix.h"
+}
+
+#define BIG_FILE "test_files/big.txt"
+#define MEDIUM_FILE "test_files/medium.txt"
+#define NORMAL_FILE "test_files/normal.txt"
+#define SMALL_FILE "test_files/small.txt"
+
+TEST(BUSINESS_LOGIC, business_logic) {
+    EXPECT_EQ(business_logic(), SUCCESS);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
