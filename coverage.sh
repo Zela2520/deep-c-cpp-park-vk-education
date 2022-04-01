59f044fbd576b9deac949d9d7cefe74985ab6c59
Zela2520 <sugarbeatzela@gmail.com>
2022-04-01 13:20:41 +0300

    everything works but no tests yet

--- coverage.sh
+++ coverage.sh
@@ -3,10 +3,11 @@
 set -e
 ./build/tests/sync_tests
 
-lcov -t "build/tests/sync_tests" "build/tests/async_tests" -o coverage.info -c -d build/tests/CMakeFiles/sync_tests.dir/ build/tests/CMakeFiles/async_tests.dir/
+lcov -t "build/tests/sync_tests" -o coverage.info -c -d build/tests/CMakeFiles/sync_tests.dir/
 genhtml -o "build/report/" coverage.info
 
-#./build/tests/async_matrix_tests
+set -e
+./build/tests/async_tests
 
-#lcov -t "build/tests/async_matrix_tests" -o coverage.info -c -d build/tests/CMakeFiles/async_matrix_tests.dir/
-#genhtml -o "build/report_2/" coverage.info
+lcov -t "build/tests/async_tests" -o coverage.info -c -d build/tests/CMakeFiles/async_tests.dir/
+genhtml -o "build/report_2/" coverage.info
