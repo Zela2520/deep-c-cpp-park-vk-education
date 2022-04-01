9f9139467685ec583633dde1348d5bb967532a68
Zela2520 <sugarbeatzela@gmail.com>
2022-04-01 12:53:32 +0300

    build running

---
+++ build/sync_lib/CMakeFiles/sequential_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sequential_lib.dir/main.c.o"
+  "sequential_lib"
+  "sequential_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sequential_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
